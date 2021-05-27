897e789622c2441b113c80ba0a1bbe24ace77487
DuyTheCoGa <47093479+zamminhduy123@users.noreply.github.com>
2021-05-27 11:33:30 +0700

    Create Invoice Models
    Add Converter for image

--- Models/BytesToBitMapConverter.cs
+++ Models/BytesToBitMapConverter.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
-namespace BakeryManager.Models
+namespace CakeShopApp.Models
 {
     public class BytesToBitMapConverter : IValueConverter
     {
